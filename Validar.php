5b0afeb1064ae191a7c247fd136e47faca6fde59
JaimeB <jaime.bustamante300@gmail.com>
2021-08-29 20:16:17 -0400

    validando nombre

---
+++ Validar.php
@@ -0,0 +1,13 @@
+<?php
+  if (isset($_POST['btn'])){
+    if (empty($nombre)){
+      echo "<p class='error'>* Falta Nombre </p>";
+    } else { 
+      if(strlen($nombre) > 15){
+
+      }
+    }
+
+  }
+
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+   if(isset($_POST['btn'])) {
+    $nombre = $_POST['Nombre'];
+    $apellido = $_POST['Apellido'];
+    $cod_tra = $_POST['Codigo'];
+    $tipo = $_POST['Usuario']; 
+  } 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+
+<form action="index.php" method="post">
+  <h1>Login</h1>
+  <label for="">Tipo de Usuario: </label>
+  <select name="Usuario" id="">
+    <option value="Productivo">Productivo</option>
+    <option value="Administrador">Administrador</option>
+  </select>
+  <label for="">Nombre: </label>
+  <input type="text" name="Nombre">
+  <label for="">Apellido: </label>
+  <input type="text" name="Apellido">
+  <label for="">Codigo trabajador: </label>
+  <input type="text" name="Codigo">
+  <input type="submit" value="Ingresar" name="btn">
+  <?php
+    include("Validar.php");
+  ?>
+</form>
+</body>
+</html>
